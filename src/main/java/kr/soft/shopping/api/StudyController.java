947a6ef133b2cd5a747e6fc8ac637d0811ddc325
MHS <hayaruby7909@gmail.com>
2025-12-23 11:01:03 +0900

    feat: data 전송

--- src/main/java/kr/soft/shopping/api/StudyController.java
+++ src/main/java/kr/soft/shopping/api/StudyController.java
@@ -1,8 +1,13 @@
 package kr.soft.shopping.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kr.soft.shopping.dto.CalcDTO;
+import kr.soft.shopping.dto.DataDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,4 +27,33 @@ public class StudyController {
         return "Hello World";
     }
 
+    //데이터를 건네는 것 부터 해봅시다. ( Input 먼저 해보자 )
+    @GetMapping("/data1")
+    public void data1(HttpServletRequest request) {
+        String data = request.getParameter("text");
+        logger.info("data: {}", data);
+    }
+
+    @GetMapping("/data2")
+    public void data2(DataDTO dto) {
+        logger.info("data: {}", dto.toString());
+    }
+
+    @GetMapping("/data3")
+    public void data3(CalcDTO calcDTO) {
+        logger.info("plus: {}", calcDTO.getNum1()+ calcDTO.getNum2());
+        logger.info("minus: {}", calcDTO.getNum1() - calcDTO.getNum2());
+    }
+
+    @PostMapping("/post1")
+    public void post1(@RequestBody DataDTO dto) {
+        logger.info("post1: {}", dto.toString());
+    }
+
+    @GetMapping("/res1")
+    public DataDTO res1(DataDTO dataDTO) {
+        logger.info("res: {}", dataDTO);
+
+        return dataDTO;
+    }
 }
---
+++ src/main/java/kr/soft/shopping/dto/DataDTO.java
@@ -0,0 +1,11 @@
+package kr.soft.shopping.dto;
+
+import lombok.Data;
+
+
+@Data   //Getter Method, Setter Method, toString Method
+public class DataDTO {
+    private String name;
+    private int age;
+
+}
